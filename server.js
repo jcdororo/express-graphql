a749a848f9ec9ff4b5ccafd7ba39116412edc9d2
jc <wnwlcks123@gmail.com>
2023-10-10 14:24:09 +0900

    Post Comment Schema 작성

--- server.js
+++ server.js
@@ -7,12 +7,50 @@ const port = 4000;
 
 const schema = buildSchema(`
   type Query {
-    description: String
+    posts: [Post],
+    comments: [Comment]
+  }
+
+  type Post {
+    id: ID!
+    title: String!
+    description: String!
+    comments: [Comment]
+  }
+
+  type Comment {
+    id: ID!
+    text: String!
+    likes: Int
   }
 `);
 
 const root = {
-  description: "hello world"
+  posts: [
+    {
+      id: 'post1',
+      title: 'It is a first post',
+      description: 'It is a first post description',
+      comments: [{
+        id: 'comment1',
+        text: 'It is a first comment',
+        likes: 1
+      }]
+    },
+    {
+      id: 'post2',
+      title: 'It is a second post',
+      description: 'It is a second post description',
+      comments: []
+    }
+  ],
+  comments: [
+    {
+      id: 'comment1',
+      text: "It is a first comment",
+      likes: 1
+    }
+  ]
 }
 
 app.use('/graphql', graphqlHTTP({
