6a517bb137694b227a9317da25f2484cdb875e04
jc <wnwlcks123@gmail.com>
2023-10-10 14:45:05 +0900

    GraphQL Tools

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 
 # express graphql
 `$ npm install express express-graphql graphql`
+
+# graphql tools / schema
+`$ npm install @graphql-tools/schema`
+
+# graphql tools / load-files
+`$ npm install @graphql-tools/load-files`
--- server.js
+++ server.js
@@ -1,56 +1,23 @@
+const { makeExecutableSchema } = require('@graphql-tools/schema');
 const express =require('express');
 const { graphqlHTTP } = require('express-graphql');
-const {buildSchema} = require('graphql');
+const { loadFilesSync } = require('@graphql-tools/load-files');
 
 const app = express();
 const port = 4000;
 
-const schema = buildSchema(`
-  type Query {
-    posts: [Post],
-    comments: [Comment]
-  }
+const loadedFiles = loadFilesSync("**/*", {
+  extensions: ['graphql']
+})
 
-  type Post {
-    id: ID!
-    title: String!
-    description: String!
-    comments: [Comment]
-  }
 
-  type Comment {
-    id: ID!
-    text: String!
-    likes: Int
-  }
-`);
+const schema = makeExecutableSchema({
+  typeDefs: loadedFiles
+})
 
 const root = {
-  posts: [
-    {
-      id: 'post1',
-      title: 'It is a first post',
-      description: 'It is a first post description',
-      comments: [{
-        id: 'comment1',
-        text: 'It is a first comment',
-        likes: 1
-      }]
-    },
-    {
-      id: 'post2',
-      title: 'It is a second post',
-      description: 'It is a second post description',
-      comments: []
-    }
-  ],
-  comments: [
-    {
-      id: 'comment1',
-      text: "It is a first comment",
-      likes: 1
-    }
-  ]
+  posts: require('./posts/posts.model'),
+  comments: require('./comments/comments.model')
 }
 
 app.use('/graphql', graphqlHTTP({
